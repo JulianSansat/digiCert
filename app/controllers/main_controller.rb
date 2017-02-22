ccc49ff795fd138a73421e9757c1b2005f3d4040
JulianSansat <kerarchie@yahoo.com.ar>
2017-02-21 22:24:09 -0300

    assinaturas

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,15 @@
 require 'open-uri'
 class MainController < ApplicationController
+  def root_get
+    @root_cas = RootCertificate.all
+    @root_ca_raw = @root_cas.first
+    @root_ca = @root_ca_raw.ca
+    #Paperclip.io_adapters.for(root_ca).read
+    @root_cert = OpenSSL::X509::Certificate.new(Paperclip.io_adapters.for(@root_ca).read)
+    @root_key_raw = @root_ca_raw.key
+    @root_key = OpenSSL::PKey::RSA.new(Paperclip.io_adapters.for(@root_key_raw).read)
+  end
+
   def index
   end
 
@@ -7,24 +17,34 @@ class MainController < ApplicationController
   end
 
   def show_cert_verification
-    root_cas = RootCertificate.all
-    root_ca_raw = root_cas.first
-    root_ca = root_ca_raw.ca
-    #Paperclip.io_adapters.for(root_ca).read
-    root_cert = OpenSSL::X509::Certificate.new(Paperclip.io_adapters.for(root_ca).read)
+    root_get
     cert = OpenSSL::X509::Certificate.new(params[:file].read)
     @subject = cert.subject.to_s
     @regular_certificate = RegularCertificate.find_by(subject: @subject)
 
     if @regular_certificate != nil
         @check_certificate = OpenSSL::X509::Certificate.new(Paperclip.io_adapters.for(@regular_certificate.ca).read)
-        @result = @check_certificate.verify(root_cert.public_key)
+        @result = @check_certificate.verify(@root_cert.public_key)
         if @result == true
         @issuer = cert.issuer
         @subject = cert.subject
         end
     end
   end
+
+  def sign_document
+
+  end
+
+  def document_signed
+        root_get
+        data = params[:file].read
+        digest = OpenSSL::Digest::SHA256.new
+        pkey = @root_key
+        signature = pkey.sign(digest, data)
+        debugger
+  end
+
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
 
   get 'main/cert_verification'
 
+  get 'main/sign_document'
+
+  post 'main/document_signed'
+
   post 'main/show_cert_verification'
+
 
 
   resources :regular_certificates
